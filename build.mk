d79254f282ad7778415784aa5943b26a018ab8f8
Jesse L. Zamora <xtremekforever@gmail.com>
2025-11-14 13:08:29 +0000

    Use LIBROOT for TOOLSET

--- build.mk
+++ build.mk
@@ -8,7 +8,7 @@ INPUT_NAME := libApplication.a
 PRJROOT := .
 LIBROOT := $(PRJROOT)/.build/checkouts/swift-stm32c011
 BUILDROOT := $(PRJROOT)/.build/$(CONFIGURATION)
-TOOLSET := $(PRJROOT)/Toolsets/stm32c0xx.json
+TOOLSET := $(LIBROOT)/Toolsets/stm32c0xx.json
 
 # Target and build flags
 ARCH := armv6m
